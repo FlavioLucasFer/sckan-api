e403b74c35c6488eae0c221e65c0409925db4a9a
flavio-lucas-fernandes <flaviolucasfer@gmail.com>
2022-01-19 01:09:57 -0300

    Improvements on CRUD of companies #54

--- app/Controllers/Http/CompaniesController.ts
+++ app/Controllers/Http/CompaniesController.ts
@@ -1,174 +1,196 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import getStream from 'get-stream';
 
-import Company from 'App/Models/Company';
+import CompaniesRepository from 'App/Repositories/CompaniesRepository';
+
 import StoreCompanyValidator from 'App/Validators/StoreCompanyValidator';
 import UpdateCompanyValidator from 'App/Validators/UpdateCompanyValidator';
 
 export default class CompaniesController {
-  public async index({ request, response }: HttpContextContract) {
-		try {
-			const { withLogo } = request.only(['withLogo']);
+  private repository = new CompaniesRepository();
+	
+	public async index({ request, response }: HttpContextContract) {
+		const {
+			columns,
+			preload,
+			trashed,
+			trashedOnly,
+			withLogo,
+			...reqQueryParams
+		} = request.only([
+			'name',
+			'tradeName',
+			'email',
+			'trashed',
+			'trashedOnly',
+			'limit',
+			'page',
+			'pageLimit',
+			'columns',
+			'preload',
+			'withLogo',
+		]);
 
-			return response.ok(await Company.customAll(withLogo));
+		const preloads = preload ? preload.split(',') : [];
+
+		try {
+			return await this.repository.all({
+				...reqQueryParams,
+				columns: columns ? columns.split(',') : null,
+				preloadUsers: preloads.includes('users'),
+				preloadProjects: preloads.includes('projects'),
+				preloadLabels: preloads.includes('labels'),
+				preloadStatuses: preloads.includes('statuses'),
+				preloadPriorities: preloads.includes('priorities'),
+				trashed: trashed === 'true',
+				trashedOnly: trashedOnly === 'true',
+				withLogo: withLogo === 'true',
+			});
 		} catch (err) {
-			return response.badRequest(err);
+			if (err?.errno)
+				return response.badRequest(err);
+
+			return response.internalServerError(err);
 		}
 	}
 
   public async store({ request, response }: HttpContextContract) {
 		try {
 			await request.validate(StoreCompanyValidator);
-
-			const {
-				name,
-				tradeName = null,
-				email,
-			} = request.only([
-				'name',
-				'tradeName',
-				'email',
-			]);
-			
-			const company = await Company.create({
-				name,
-				tradeName,
-				email,
-			});
-
-			return response.created(company);
 		} catch (err) {
 			return response.badRequest(err);
+		}
+
+		const reqBody = request.only([
+			'name',
+			'tradeName',
+			'email',
+		]);
+
+		try {
+			return response.created(
+				await this.repository.persist(reqBody),
+			);
+		} catch (err) {
+			return response.internalServerError(err);
 		}
 	}
 
   public async show({ params, request, response }: HttpContextContract) {
-		try {
-			const { id } = params;
-			const { withLogo } = request.only(['withLogo']);
+		const { id } = params;
+		const { 
+			columns,
+			preload,
+			withLogo,
+		} = request.only([
+			'columns',
+			'preload',
+			'withLogo',
+		]);
 
-			return response.ok(await Company.customFindOrFail(id, withLogo));
+		const preloads = preload ? preload.split(',') : [];
+		
+		try {
+			return await this.repository.findOrFail(id, {
+				columns: columns ? columns.split(',') : null,
+				withLogo: withLogo === 'true',
+				preloadUsers: preloads.includes('users'),
+				preloadProjects: preloads.includes('projects'),
+				preloadLabels: preloads.includes('labels'),
+				preloadStatuses: preloads.includes('statuses'),
+				preloadPriorities: preloads.includes('priorities'),
+			});
 		} catch (err) {
 			if (err.code === 'E_ROW_NOT_FOUND')
-				return response.notFound({
-					code: err.code,
-					message: 'Record not found.',
-				});
+				return response.notFound(err);
 
-			return response.badRequest(err);
+			if (err?.errno)
+				return response.badRequest(err);
+
+			return response.internalServerError(err);
 		}
 
 	}
 
 	public async logo({ params, request, response }: HttpContextContract) {
+		const { id } = params;
+		let logo: Buffer | null = null;
+		
 		try {
-			const { id } = params;
-			let logo: Buffer | null = null;
-
 			request.multipart.onFile('logo', {
 				size: '2mb',
 				extnames: ['jpg', 'png', 'jpeg'],
 			}, async file => {
 				logo = await getStream.buffer(file);
 			});
+		} catch (err) {
+			return response.badRequest(err);
+		}
 
+		try {
 			await request.multipart.process();
+		} catch (err) {
+			return response.internalServerError(err);
+		}
 
-			const company = await Company.findOrFail(id);
-
-			company.logo = logo;
-
-			await company.save();
-
-			return response.ok(company);
+		try {
+			return await this.repository.logo(id, logo);
 		} catch (err) {
 			if (err.code === 'E_ROW_NOT_FOUND')
-				return response.notFound({
-					code: err.code,
-					message: 'Record not found.',
-				});
+				return response.notFound(err);
 
-			return response.badRequest(err);
+			return response.internalServerError(err);
 		}
 	}
 
   public async update({ params, request, response }: HttpContextContract) {
-		try {
-			
-			const { id } = params;
-			
-			const company = await Company.customFindOrFail(id, false);
-			
+		const { id } = params;
+
+		try {				
 			await request.validate(UpdateCompanyValidator);
-			
-			const {
-				name,
-				tradeName,
-				email,
-			} = request.only([
-				'name',
-				'tradeName',
-				'email',
-			]);
+		} catch (err) {
+			return response.badRequest(err);
+		}
 
-			if (name)
-				company.name = name;
-			if (tradeName)
-				company.tradeName = tradeName;
-			if (email)
-				company.email = email;
+		const reqBody = request.only([
+			'name',
+			'tradeName',
+			'email',
+		]);
 
-			await company.save();
-
-			return response.ok(company);
+		try {
+			return this.repository.update({ id, ...reqBody });
 		} catch (err) {
 			if (err.code === 'E_ROW_NOT_FOUND')
-				return response.notFound({
-					code: err.code,
-					message: 'Record not found.',
-				});
+				return response.notFound(err);
 
-			return response.badRequest(err);
+			return response.internalServerError(err);
 		}
 	}
 
   public async destroy({ params, response }: HttpContextContract) {
+		const { id } = params;
+
 		try {
-			const { id } = params;
-
-			const company = await Company.findOrFail(id);
-
-			await company.softDelete();
-
-			return response.ok(true);
+			return await this.repository.delete(id);
 		} catch (err) {
 			if (err.code === 'E_ROW_NOT_FOUND')
-				return response.notFound({
-					code: err.code,
-					message: 'Record not found.',
-				});
+				return response.notFound(err);
 
-			return response.badRequest(err);
+			return response.internalServerError(err);
 		}
 	}
 
 	public async restore({ params, response }: HttpContextContract) {
+		const { id } = params;
+
 		try {
-			const { id } = params;
-
-			const company = await Company.findOnlyTrashedOrFail(id);
-
-			await company.restore();
-
-			return response.ok(true);
+			return await this.repository.restore(id);
 		} catch (err) {
 			if (err.code === 'E_ROW_NOT_FOUND')
-				return response.notFound({
-					code: err.code,
-					message: 'No deleted record found.',
-				});
+				return response.notFound(err);
 
-			return response.badRequest(err);
+			return response.internalServerError(err);
 		}
 	}
 }
---
+++ app/Repositories/CompaniesRepository.ts
@@ -0,0 +1,282 @@
+import { ModelObject } from "@ioc:Adonis/Lucid/Orm";
+
+import Company from "App/Models/Company";
+
+export type PersistCompanyFields = {
+	name: string,
+	tradeName?: string,
+	email: string,
+};
+
+export type UpdateCompanyFields = {
+	id: number,
+	name?: string,
+	tradeName?: string,
+	email?: string,
+};
+
+export type FindOrFailQueryParams = {
+	columns?: string[],
+	trashed?: boolean,
+	trashedOnly?: boolean,
+	withLogo?: boolean,
+	preloadUsers?: boolean,
+	preloadProjects?: boolean,
+	preloadLabels?: boolean,
+	preloadStatuses?: boolean,
+	preloadPriorities?: boolean,
+};
+
+export type AllQueryParams = FindOrFailQueryParams & {
+	name?: string,
+	tradeName?: string,
+	email?: string,
+	limit?: number,
+	page?: number,
+	pageLimit?: number,
+};
+
+interface CompaniesRepositoryInterface {
+	persist(fields: PersistCompanyFields): Promise<Company>;
+	update(fields: UpdateCompanyFields): Promise<Company>;
+	logo(id: number, logo: Buffer | null): Promise<Company>;
+	delete(id: number): Promise<boolean>;
+	restore(id: number): Promise<boolean>;
+	all(params?: AllQueryParams): Promise<Company[] | { meta: any; data: ModelObject[]; }>;
+	findOrFail(id: number, params?: FindOrFailQueryParams): Promise<Company>;
+};
+
+export default class CompaniesRepository implements CompaniesRepositoryInterface {
+	private columns: string[];
+
+	public constructor() {
+		const columns = Array.from(Company.$columnsDefinitions, ([key]) => key);
+		columns.splice(columns.indexOf('deletedAt'), 1);
+		columns.splice(columns.indexOf('logo'), 1);
+
+		this.columns = columns;
+	}
+	
+	public async persist(fields: PersistCompanyFields): Promise<Company> {
+		try {
+			return await Company.create(fields);
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	public async update(fields: UpdateCompanyFields): Promise<Company> {
+		const {
+			id,
+			name,
+			tradeName,
+			email,
+		} = fields;
+		let company: Company;
+
+		try {
+			company = await this.findOrFail(id);
+		} catch (err) {
+			throw err;
+		}
+
+		if (name)
+			company.name = name;
+
+		if (tradeName)
+			company.tradeName = tradeName;
+
+		if (email)
+			company.email = email;
+
+		try {
+			await company.save();
+
+			return await company.refresh();
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	public async logo(id: number, logo: Buffer | null): Promise<Company> {
+		let company: Company;
+
+		try {
+			company = await this.findOrFail(id);
+		} catch (err) {
+			throw err;
+		}
+
+		company.logo = logo;
+
+		try {
+			await company.save();
+		} catch (err) {
+			throw err;
+		}
+
+		return await company.refresh();
+	}
+
+	public async delete(id: number): Promise<boolean> {
+		let company: Company;
+
+		try {
+			company = await this.findOrFail(id);
+		} catch (err) {
+			throw err;
+		}
+
+		try {
+			await company.softDelete();
+
+			return true;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	public async restore(id: number): Promise<boolean> {
+		let company: Company;
+
+		try {
+			company = await this.findOrFail(id, { trashedOnly: true });
+		} catch (err) {
+			throw err;
+		}
+
+		try {
+			await company.restore();
+
+			return true;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	public async all(params?: AllQueryParams): Promise<Company[] | { meta: any; data: ModelObject[]; }> {
+		const query = Company.query()
+			.select(params?.columns || this.columns)
+
+		if (params) {
+			const {
+				name,
+				tradeName,
+				email,
+				trashed = false,
+				trashedOnly = false,
+				withLogo = false,
+				preloadUsers = false,
+				preloadProjects = false,
+				preloadLabels = false,
+				preloadStatuses = false,
+				preloadPriorities = false,
+				limit,
+			} = params;
+
+			if (withLogo)
+				query.select('logo');
+
+			if (trashed && !trashedOnly)
+				query.withTrashed();
+
+			else if (!trashed && trashedOnly)
+				query.onlyTrashed();
+
+			if (name)
+				query.where('name', 'LIKE', `%${name}%`);
+
+			if (tradeName)
+				query.where('tradeName', 'LIKE', `%${tradeName}%`);
+
+			if (email)
+				query.where('email', 'LIKE', `%${email}%`);
+
+			if (preloadUsers)
+				query.preload('users');
+
+			if (preloadProjects)
+				query.preload('projects');
+
+			if (preloadLabels)
+				query.preload('labels');
+
+			if (preloadStatuses)
+				query.preload('statuses');
+
+			if (preloadPriorities)
+				query.preload('priorities');
+
+			if (limit)
+				query.limit(limit);
+		}
+
+		try {
+			if (params?.page)
+				return (await query.paginate(params.page, params.pageLimit || 10)).toJSON();
+
+			return await query;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	public async findOrFail(id: number, params?: FindOrFailQueryParams): Promise<Company> {
+		let recordNotFoundMessage = 'Company not found';
+
+		const query = Company.query()
+			.select(params?.columns || this.columns)
+			.where('id', id);
+
+		if (params) {
+			const {
+				trashed = false,
+				trashedOnly = false,
+				withLogo = false,
+				preloadUsers = false,
+				preloadProjects = false,
+				preloadLabels = false,
+				preloadStatuses = false,
+				preloadPriorities = false,
+			} = params;
+
+			if (withLogo)
+				query.select('logo');
+
+			if (trashed && !trashedOnly)
+				query.withTrashed();
+
+			else if (!trashed && trashedOnly) {
+				query.onlyTrashed();
+				recordNotFoundMessage = 'No deleted company found';
+			}
+
+			if (preloadUsers)
+				query.preload('users');
+
+			if (preloadProjects)
+				query.preload('projects');
+
+			if (preloadLabels)
+				query.preload('labels');
+
+			if (preloadStatuses)
+				query.preload('statuses');
+
+			if (preloadPriorities)
+				query.preload('priorities');
+		}
+
+		try {
+			return await query.firstOrFail();
+		} catch (err) {
+			if (err.code === 'E_ROW_NOT_FOUND')
+				throw {
+					code: err.code,
+					message: recordNotFoundMessage,
+				};
+
+			throw err;
+		}
+	}
+};
